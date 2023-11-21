db0273f4c9fccb292056e0336117340673e4300c
choikkk <sixballoons@naver.com>
2023-11-21 21:38:33 +0900

    1121

--- choikkk/src/App.jsx
+++ choikkk/src/App.jsx
@@ -1,9 +1,15 @@
+import { NavLink } from "react-router-dom";
 import ListPage from "./pages/ListPage"
 import WritePage from "./pages/WritePage"
 
 function App() {
   return (
     <>
+      <header>
+        <NavLink to="list" />
+        <li>리스트</li>
+        <li>작성</li>
+      </header>
       <ListPage />
       <WritePage />
     </>
