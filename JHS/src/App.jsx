4b244fd67b03284c6754281230d05f4f7d39d6ff
bugrice <manssik0820@gmail.com>
2023-11-21 21:24:08 +0900

    1121sik

--- JHS/src/App.jsx
+++ JHS/src/App.jsx
@@ -1,12 +1,21 @@
 import ListPage from "./pages/ListPages";
 import WritePage from "./pages/WritePage";
+import {Navigate, Route, Routes} from "react-router-dom";
 
 function App() {
   
   return (
     <>
-      <ListPage />
-      <WritePage/>
+      <header>
+        <a href="/list">리스트</a>
+        /
+        <a href="/wirte">작성</a>
+      </header>
+      {/* <Routes>
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/write" element={<WritePage/>} />
+        <Route path="*" element={<Navigate to="/list"/>}/>
+      </Routes> */}
     </>
   )
 }
