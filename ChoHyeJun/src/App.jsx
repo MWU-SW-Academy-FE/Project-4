f34bd751c22408415d6bdd8023d4ce7312b7835e
hyedevj <hyedevj@gmail.com>
2023-11-22 15:42:50 +0900

    라우터 도입

--- ChoHyeJun/src/App.jsx
+++ ChoHyeJun/src/App.jsx
@@ -1,13 +1,22 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import ListPage from "./pages/ListPage"
 import WritePage from "./pages/WritePage"
 
 function App() {
-  return (
-    <>
-      <ListPage />
-      <WritePage/>
-    </>
-  )
+	return (
+		<>
+			<header>
+        <a href="/list">리스트</a>
+        //
+				<a href="/write">작성</a>
+			</header>
+      {/* <Routes>
+        <Route path="/list" element={<ListPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="*" element={<Navigate to="/list" />} />
+  </Routes> */}
+		</>
+	);
 }
 
-export default App
+export default App;
