3af83200f4a2ca5e8707cc443ba126c799c43912
mwssy <mokmail070@gmail.com>
2023-11-22 14:34:46 +0900

    page 컴포넌트

--- SSY/src/App.jsx
+++ SSY/src/App.jsx
@@ -1,7 +1,12 @@
+import ListPage from "./assets/pages/ListPage"
+import WritePage from "./assets/pages/WritePage"
+
 function App() {
 
   return (
     <>
+      <ListPage></ListPage>
+      <WritePage></WritePage>
     </>
   )
 }
---
+++ SSY/src/assets/pages/WritePage.jsx
@@ -0,0 +1,9 @@
+function WritePage() {
+    return (
+        <>
+            <h1>작성페이지</h1>
+        </>
+    )
+}
+
+export default WritePage;
