f24210ecc9b69d340c28fc336986fd1083f82b30
yan754 <yan754@naver.com>
2023-11-23 15:19:46 +0900

    실습중

--- soeun/src/App.jsx
+++ soeun/src/App.jsx
@@ -1,12 +1,25 @@
-import ListPage from "./pages/ListPage.jsx"
-import WritePage from "./pages/WritePage.jsx"
+import {Navigate, Route, Routes} from "react-router-dom";
+import ListPage from "./pages/ListPage"
+import WritePage from "./pages/WritePage"
 
 function App() {
    
     return (
         <>
-          <ListPage />
-          <WritePage />
+          <header>
+            
+            <a href="/list">리스트</a>
+            <a href="/write">작성</a>
+
+          </header> 
+
+         {/*
+            <Routes>
+              <Route path = "/list" element = {<ListPage />} /> 
+              <Route path = "/write" element = {<WritePage/>} /> 
+              <Route path = "*" element= {<Navigate to = "/list"/>}/> 
+
+            </Routes> */}
 
         </>
     )
