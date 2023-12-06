158ab8ce5860d6a3fbe3757d738497b3e236e5f4
hyedevj <hyedevj@gmail.com>
2023-12-06 16:10:43 +0900

    라우터, 테일윈드 설정

--- ChoHyeJun/src/App.jsx
+++ ChoHyeJun/src/App.jsx
@@ -1,20 +1,23 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import ListPage from "./pages/ListPage"
-import WritePage from "./pages/WritePage"
+import { Routes, Route, Navigate, NavLink } from "react-router-dom";
+import ListPage from "./pages/ListPage";
+import WritePage from "./pages/WritePage";
+
 
 function App() {
 	return (
 		<>
+			<h1 className="text-3xl font-bold underline">
+				Hello World
+			</h1>
 			<header className="flex">
-        <p className="cursor-pointer" href="/list">리스트</p>
-        //
-				<a className="font-bold p-3 hover:text-red-500" href="/write">작성</a>
+				<NavLink className="font-bold" to="/list">리스트</NavLink>
+				<NavLink className="font-bold" to="/write">작성</NavLink>
 			</header>
-      {/* <Routes>
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/write" element={<WritePage />} />
-        <Route path="*" element={<Navigate to="/list" />} />
-  </Routes> */}
+			<Routes>
+				<Route path="/list" element={<ListPage />} />
+				<Route path="/write" element={<WritePage />} />
+				<Route path="*" element={<Navigate to="/list" />} />
+			</Routes>
 		</>
 	);
 }
