65c110e82129a5a0e2bc4cebdee1be3687f624ed
hyedevj <hyedevj@gmail.com>
2023-12-14 15:49:42 +0900

    CountNumber, RecordModal, hooks 작업

--- ChoHyeJun/src/components/RecordModal.jsx
+++ ChoHyeJun/src/components/RecordModal.jsx
@@ -2,10 +2,25 @@ import { Button, Modal } from "@mui/material"
 import { useEffect, useState } from "react";
 import { myConfetti } from "../utill";
 
-export default function RecordModal({
+export function useRecordModalStatus() {
+	const [isOpen, setIsOpen] = useState(false)
+
+	const onClose = () => setIsOpen(false)
+	const onOpen = () => setIsOpen(true)
+	
+	return {
+		isOpen,
+		onClose,
+		onOpen
+		}
+	}
+
+export function RecordModal({
 	status,
 	msg,
-	initialQuantity = 0
+	initialQuantity = 0,
+	// saveRecord: _saveRecord,
+	// cancelRecord: _saveRecord
 }) {
 	const [recordCount, setRecordCount] = useState(initialQuantity);
 
@@ -16,19 +31,28 @@ export default function RecordModal({
 	const changeRecordCount = (count) => {
 		if (count > 0) {
 			myConfetti({
-				particleCount: count * 100,
+				particleCount: count * 10,
 				spreed: 160,
 			});
 		}
-		setRecordCount(count)
-	}
+	};
+
+	const saveRecord = () => {
+		setRecordCount(0);
+		status.onClose();
+	};
+
+	const cancelRecord = () => {
+		setRecordCount(0);
+		status.onClose();
+	};
 
 	return (
 		<>
 			<Modal
 				className="flex justify-center items-center"
-				open={true}
-				// onClose={cancelRecord}
+				open={status.isOpen}
+				onClose={cancelRecord}
 			>
 				<div className="bg-white rounded-[20px] p-7 w-full max-w-lg">
 					<div className="text-center select-none">{msg}</div>
@@ -38,14 +62,26 @@ export default function RecordModal({
 						</span>
 					</div>
 					<div className="flex justify-center gap-2">
-						<Button variant="contained" onClick={() => changeRecordCount(5)}>+ 5</Button>
-						<Button variant="contained" onClick={() => changeRecordCount(1)}>+ 1</Button>
-						<Button variant="contained" onClick={() => changeRecordCount(-1)}>- 1</Button>
-						<Button variant="contained" onClick={() => changeRecordCount(-5)}>- 5</Button>
+						<Button variant="contained" onClick={() => changeRecordCount(5)}>
+							+ 5
+						</Button>
+						<Button variant="contained" onClick={() => changeRecordCount(1)}>
+							+ 1
+						</Button>
+						<Button variant="contained" onClick={() => changeRecordCount(-1)}>
+							- 1
+						</Button>
+						<Button variant="contained" onClick={() => changeRecordCount(-5)}>
+							- 5
+						</Button>
 					</div>
 					<div className="mt-10 flex justify-center gap-2">
-						<Button variant="contained">적용</Button>
-						<Button variant="outlined">취소</Button>
+						<Button variant="contained" onClick={saveRecord}>
+							적용
+						</Button>
+						<Button variant="outlined" onClick={cancelRecord}>
+							취소
+						</Button>
 					</div>
 				</div>
 			</Modal>
--- ChoHyeJun/src/pages/Main.jsx
+++ ChoHyeJun/src/pages/Main.jsx
@@ -1,33 +1,45 @@
-import { useRecoilState } from "recoil";
-import { todosAtom } from "../states";
-import RecordModal from "../components/RecordModal";
+import { useState } from "react";
+import { RecordModal, useRecordModalStatus } from "../components/RecordModal";
+import { Button } from "@mui/material";
+import useRecordStatus from "../hooks";
+import CountNumber from "../components/CountNumber";
 
-function RecordAddModal() {
-	const status = {opened: true}
+function RecordAddModal({ status }) {
+
+	const recordState = useRecordStatus();
+
+	const saveRecord = (record) => {
+
+	}
+
 	return (
 		<>
 			<RecordModal
 				msg="몇회 진행하셨나요?"
 				status={status}
+				saveRecord={saveRecord}
 			/>
 		</>
-	)
+	);
 }
 
 export default function Main() {
-  //const [todos, setTodos] = useRecoilState(todosAtom)
+	const recordModalStatus = useRecordModalStatus();
 
-  return (
+	return (
 		<>
-			<RecordAddModal />
-			<div className="flex-1 flex items-center justify-center"></div>
-			<div className="text-[70px] text-[color:gray] select-none">
-				<span>10</span>
-			</div>
-			<div className="flex justify-center">
-				<button className="btn btn-accent" variant="contained" onClick="{}">
-					스쿼트 횟수 기록하기
-				</button>
+			<RecordAddModal status={recordModalStatus} />
+			<div className="flex-1 flex items-center justify-center">
+				<div>
+					<div className="text-[70px] text-[color:var(--mui-color-primary-main)] font-mono select-none">
+						<CountNumber start={0} end={100} duration={3} />
+					</div>
+					<div className="flex justify-center">
+						<Button variant="contained" onClick={recordModalStatus.onOpen}>
+							스쿼트 횟수 기록하기
+						</Button>
+					</div>
+				</div>
 			</div>
 		</>
 	);
