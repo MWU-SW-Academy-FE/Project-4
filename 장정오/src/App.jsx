bed96ba0cdf55b06e1c84faacfbbde6e78095150
k1163172 <k1163172@naver.com>
2023-12-14 16:40:04 +0900

    네비 변경

--- 장정오/src/App.jsx
+++ 장정오/src/App.jsx
@@ -1,21 +1,18 @@
-import { NavLink, Navigate, Route, Routes } from "react-router-dom";
-import ListPage from "./pages/ListPage"
-import WritePage from "./pages/WritePage"
+import { Navigate, Route, Routes } from "react-router-dom";
+import History from "./pages/History"
+import Main from "./pages/Main"
+import Nav from "./components/Nav";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <header className="flex">
-        <NavLink className="font-bold" to="/list">리스트</NavLink>
-        <NavLink className="font-bold" to="/write">작성</NavLink>
-      </header>
+      <Nav />
+      <Button variant="contained">123</Button>
       <Routes>
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/write" element={<WritePage />} />
-        <Route path="*" element={<Navigate to="list" />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="*" element={<Navigate to="/main" />}/>
       </Routes>
     </>
   )
